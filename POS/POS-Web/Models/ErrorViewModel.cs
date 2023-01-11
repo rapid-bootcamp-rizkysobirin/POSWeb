831b64ac458edefefae72240e0364becf4b9e2c6
natadekoko <60341975+natadekoko-rin@users.noreply.github.com>
2023-01-11 09:04:57 +0700

    ubah tampilan default

---
+++ POS/POS-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POS_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
